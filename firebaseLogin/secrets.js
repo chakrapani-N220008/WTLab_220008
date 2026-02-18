e530370c2ffe0ad340a4b0b813838abf97504168
chakrapani-N220008 <n220008@rguktn.ac.in>
2026-02-18 14:41:02 +0530

    Removed Firebase API key from repository

--- firebaseLogin/secrets.js
+++
@@ -1,6 +0,0 @@
- export const firebaseConfig={
-apiKey: "AIzaSyD5bQTHL4Xu7RBh06xRAXPK5gJU-dQUiKg",
-authDomain: "shopkart-23d49.firebaseapp.com",
-projectId: "shopkart-23d49",
-appId:"1:690738857113:web:b6e2b7f3c15c5ab8b72408"
- }
